85c0beae5be5ecf67c554724aef4062931e941a0
Alberto Salazar Zuñiga <83187414+alversalazar@users.noreply.github.com>
2023-10-25 18:05:33 -0600

    Add files via upload

---
+++ Practicas/consola seleccion Region c#/consola seleccion Region c#/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static DataSet ds = new DataSet();
+    static SqlDataAdapter da = new SqlDataAdapter();
+    static SqlConnection conexion;
+    static string consulta;
+
+    static void Main(string[] args)
+    {
+        string cadena = "Data Source=DESKTOP-KE2G5CS\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;TrustServerCertificate=True";
+
+        conexion = new SqlConnection(cadena);
+        conexion.Open();
+
+        consulta = "select * from Region order by RegionID";
+        da = new SqlDataAdapter(consulta, conexion);
+        da.Fill(ds, "Region");
+
+        foreach (DataRow r in ds.Tables["Region"].Rows)
+        {
+            for (int c = 0; c < ds.Tables["Region"].Columns.Count; c++)
+            {
+                Console.Write(r.ItemArray[c].ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+
+        conexion.Close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
